af5cda6580910b5f5c744d150c3e5a839c5380a5
Tibo <tiborkeri@icloud.com>
2019-07-07 12:26:44 +0100

    Updates

---
+++ precache-manifest.6f0880384e1e16698e80fd021ed30b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acd3b8dcb4adc6bee367354f87b77e2",
+    "url": "/reactplayer/index.html"
+  },
+  {
+    "revision": "4e7c9079784a7a705493",
+    "url": "/reactplayer/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "6ab34f0301202158deda",
+    "url": "/reactplayer/static/css/main.8f77f0f5.chunk.css"
+  },
+  {
+    "revision": "4e7c9079784a7a705493",
+    "url": "/reactplayer/static/js/2.f35b7fc2.chunk.js"
+  },
+  {
+    "revision": "6ab34f0301202158deda",
+    "url": "/reactplayer/static/js/main.5a4eb1eb.chunk.js"
+  },
+  {
+    "revision": "9cfa2485844cb646d73d",
+    "url": "/reactplayer/static/js/runtime~main.7de1978d.js"
+  }
+]);
